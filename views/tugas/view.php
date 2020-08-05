410ae862e3d411717d0aa9939cfd213730642ecc
SarwanHamid <srwnhamid@gmail.com>
2020-08-05 11:07:21 +0700

    detail tugas

--- views/tugas/view.php
+++ views/tugas/view.php
@@ -10,6 +10,7 @@ $this->title = $model->nama_tugas;
 $this->params['breadcrumbs'][] = ['label' => 'Tugas', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
+$connection = Yii::$app->getDb();
 ?>
 <div class="tugas-view">
 
@@ -25,4 +26,45 @@ $this->params['breadcrumbs'][] = $this->title;
         ],
     ]) ?>
 
+<?php
+
+
+$command = $connection->createCommand("SELECT sis.nis,sis.nama,td.tanggal_upload,td.nama_file,td.nilai FROM tugas_detail as td JOIN siswa as sis ON td.nis=sis.nis");
+
+$result = $command->queryAll();
+?>
+<table class="table table-responsive">
+    <thead class="bg-blue-gradient">
+        <tr>
+            <th scope="col">NO</th>
+            <th scope="col">NAMA SISWA</th>
+            <th scope="col">NIS</th>
+            <th scope="col">TANGGAL UPLOAD</th>
+            <th scope="col">NAMA FILE</th>
+            <th scope="col">NILAI</th>
+            <th scope="col">AKSI</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tbody>
+            <?php
+            $no = 1;
+            foreach ($result as $data) {
+
+            ?>
+                <tr>
+                    <th scope="row"><?= $no; ?></th>
+                    <td><?= $data['nama']; ?></td>
+                    <td><?= $data['nis']; ?></td>
+                    <td><?= $data['tanggal_upload']; ?></td>
+                    <td><?= $data['nama_file']; ?></td>
+                    <td><?= $data['nilai']; ?></td>
+                    
+                </tr>
+            <?php
+                $no++;
+            }
+            ?>
+        </tbody>
+</table>
 </div>
